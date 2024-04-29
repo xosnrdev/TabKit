978f4b5feacdfb8772bf37283b12ca9af3bafdfd
xosnrdev <successxodev@gmail.com>
2024-04-29 09:08:11 -0400

    feat: on a mission to make TabKit even better for you and me

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,15 +1,15 @@
 import { defineConfig } from "tsup";
 
 export default defineConfig({
-	entry: ["src/index.ts"],
-	format: ["esm", "cjs"],
-	splitting: false,
-	sourcemap: true,
-	clean: true,
-	minify: "terser",
-	treeshake: true,
-	outDir: "lib",
-	shims: true,
-	skipNodeModulesBundle: true,
-	dts: true
+  entry: ["src/index.ts"],
+  format: ["esm", "cjs"],
+  splitting: false,
+  sourcemap: true,
+  clean: true,
+  minify: "terser",
+  treeshake: true,
+  outDir: "lib",
+  shims: true,
+  skipNodeModulesBundle: true,
+  dts: true,
 });
