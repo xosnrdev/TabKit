1b903df897558e044ed3b2d2a1bbb345c0eaf23d
xosnrdev <successxodev@gmail.com>
2024-04-29 09:20:40 -0400

    feat: still on a mission to make TabKit even better for you and me

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import { TabProvider } from "./index.ts";
 import "./index.css";
+import React from "react";
 
 createRoot(document.getElementById("root")!).render(
 	<StrictMode>
